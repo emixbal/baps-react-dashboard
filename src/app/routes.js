a761d4c780eb917c94630125f04c2b2daa8fc961
iqbal <emixbal@gmail.com>
2019-09-14 14:57:15 +0700

    create routes file

---
+++ src/app/routes.js
@@ -0,0 +1,12 @@
+import Index from "screens/Index.jsx";
+
+var routes = [
+  {
+    path: "/index",
+    name: "Dashboard",
+    icon: "ni ni-tv-2 text-primary",
+    component: Index,
+    layout: "/admin"
+  },
+];
+export default routes;
