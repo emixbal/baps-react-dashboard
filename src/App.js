eff1f43198fa54f55647b5372858f647e172d4a9
iqbal <emixbal@gmail.com>
2019-09-14 14:57:06 +0700

    make routes in app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Admin from "./app/layouts/Admin"
+import Auth from "./app/layouts/Auth"
+
+function Home(){
+    return(
+      <div>
+        Home
+      </div>
+    )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-          Open modal
-        </button>
-      </header>
-      <div class="modal fade" id="myModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
-
-            <div class="modal-header">
-              <h4 class="modal-title">Modal Heading</h4>
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-
-            <div class="modal-body">
-              Modal body..
-            </div>
-
-            <div class="modal-footer">
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            </div>
-
-          </div>
-        </div>
-      </div>      
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/admin" component={Admin} />
+      <Route path='/auth' component={Auth} />
+    </Router>
   );
 }
 
 export default App;
+
+// const Topics = ({ match }) => (
+//   <div>
+//     <h2>Topics</h2>
+//     <Link to={`${match.url}/exampleTopicId`}>
+//       Example topic
+//     </Link>
+//     <Route path={`${match.url}/exampleTopicId`} component={Topic}/>
+//   </div>
+// )
