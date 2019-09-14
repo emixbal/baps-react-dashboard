6c82b1395f0179087d6c9ea03806b10834824f31
iqbal <emixbal@gmail.com>
2019-09-14 14:57:36 +0700

    create auth layout

---
+++ src/app/layouts/Auth.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Auth extends React.Component {
+    render(){
+        return(
+            <>
+                <p>
+                    Halaman Auth
+                </p>
+            </>
+        )
+    } 
+}
+
+export default Auth;
