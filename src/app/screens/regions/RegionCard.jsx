a338d8ecef362dd3b3abccee7816f66e69bdaf02
iqbal <emixbal@gmail.com>
2019-09-27 13:42:31 +0700

    init region card

---
+++ src/app/screens/regions/RegionCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class RegionCard extends Component {
+    render(){
+        return(
+            <div className="col-sm-3 mb-sm-3">
+                <div className="card">
+                    <div className="card-body">Basic card</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default RegionCard;
