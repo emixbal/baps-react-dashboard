ed84d0bcf2ac8d1dac220f66ecf64149ed8f3bfd
iqbal <emixbal@gmail.com>
2019-09-27 17:36:23 +0700

    fix login page

--- src/app/screens/login/index.jsx
+++ src/app/screens/login/index.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+
+import { login } from '../../redux/actions/login';
 
 class LoginScreen extends Component{
-    render(){
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         email:'',
+    //         password:''
+    //     }
+    // }
+    state = {
+        email:'',
+        password:''
+    }
+
+    handleLoginButton() {
+        alert(JSON.stringify(this.state))
+        // this.props.dispatch(login(user));
+    }
+    onChange = e => this.setState({ [e.target.name]: e.target.value })
+
+    render(){        
         return(
             <div className="container">
                 <div className="content">
@@ -14,14 +35,31 @@ class LoginScreen extends Component{
                                 </div>
                                 <div className="card-body">
                                     <div className="form-group">
-                                        <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                        <label >Email address</label>
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            className="form-control"
+                                            id="exampleInputEmail1"
+                                            aria-describedby="emailHelp"
+                                            placeholder="Enter email"
+                                            value={this.state.email}
+                                            onChange={this.onChange}
+                                        />
                                     </div>
                                     <div className="form-group">
-                                        <label for="exampleInputPassword1">Password</label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                        <label >Password</label>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            className="form-control"
+                                            id="exampleInputPassword1"
+                                            placeholder="Password"
+                                            value={this.state.password}
+                                            onChange={this.onChange}
+                                        />
                                     </div>
-                                    <Link className="btn btn-primary">Login</Link>
+                                    <button className="btn btn-primary" onClick={this.handleLoginButton}>Login</button>
                                 </div>
                             </div>
                         </div>
@@ -32,4 +70,8 @@ class LoginScreen extends Component{
     }
 }
 
-export default LoginScreen;
+const mapStateToPros = state => ({
+    region: state.region
+})
+
+export default connect(mapStateToPros)(LoginScreen);
