91abc22d27debc87b7c7a93af39658547acad4f9
iqbal <emixbal@gmail.com>
2019-09-27 16:49:10 +0700

    looing sidebar

--- src/app/components/AdminSidebar.js
+++ src/app/components/AdminSidebar.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import routes from "../routes";
 
 class AdminSidebar extends Component {
+    // create links function 
+    createLinks = routes => {
+        return routes.map((prop, key) => {
+            return (
+                <div key={key}>
+                    <Link
+                        className="list-group-item list-group-item-action bg-light"
+                        to={prop.layout + prop.path}
+                        onClick={this.closeCollapse}
+                        activeClassName="active"
+                    >
+                        {prop.name}
+                    </Link>
+                </div>
+            );
+        });
+    };
+
     render(){
         return(
             <div className="bg-light border-right" id="sidebar-wrapper">
                 <div className="sidebar-heading">Start Bootstrap </div>
                 <div className="list-group list-group-flush">
-                    <Link to="" className="list-group-item list-group-item-action bg-light">Dashboard</Link>
+                    {this.createLinks(routes)}
+                    {/* <Link to="" className="list-group-item list-group-item-action bg-light">Dashboard</Link>
                     <Link to="" className="list-group-item list-group-item-action bg-light">Shortcuts</Link>
                     <Link to="" className="list-group-item list-group-item-action bg-light">Overview</Link>
                     <Link to="" className="list-group-item list-group-item-action bg-light">Events</Link>
                     <Link to="" className="list-group-item list-group-item-action bg-light">Profile</Link>
-                    <Link to="" className="list-group-item list-group-item-action bg-light">Status</Link>
+                    <Link to="" className="list-group-item list-group-item-action bg-light">Status</Link> */}
                 </div>
             </div>
         )
