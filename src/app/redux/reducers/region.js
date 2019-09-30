5a010cab1bc7ea807d2597263354b0f0c9377323
iqbal <emixbal@gmail.com>
2019-09-30 17:00:54 +0700

    add more reducers

--- src/app/redux/reducers/region.js
+++ src/app/redux/reducers/region.js
@@ -2,12 +2,16 @@ const initialState = {
     data: [],
     isLoading: false,
     isFinish: false,
-    isError: false
+    isError: false,
+    statusCode:'',
+    errorMessage:'',
+    isNetworkError:false,
+    regionDetail:{}
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        // case login
+        // case create a new region
         case "FETCH_ALL_PENDING":
             return {
                 ...state, isLoading: true
@@ -16,13 +20,79 @@ export default (state = initialState, action) => {
         case "FETCH_ALL_FULFILLED":
             return {
                 ...state,
-                isLoading: false, isFinish: true,
-                data: action.payload.data.data
+                isLoading: false,
+                isFinish: true,
+                data: action.payload.data
             }
 
         case "FETCH_ALL_REJECTED":
+            if(typeof(action.payload.response)!=='undefined'){
+                return {
+                    ...state,
+                    statusCode:action.payload.response.status,
+                    errorMessage:action.payload.response.data.message,
+                    isError: true,
+                    isLoading: false
+                }
+            }
             return {
-                ...state, isError: true, isLoading: false
+                ...state, isError: true, isLoading: false, isNetworkError: true,
+            }
+
+        // case create a new region
+        case "CREATE_PENDING":
+            return {
+                ...state, isLoading: true
+            }
+
+        case "CREATE_FULFILLED":
+            return {
+                ...state,
+                isLoading: false,
+                isFinish: true,
+                regionDetail: action.payload.data
+            }
+
+        case "CREATE_REJECTED":
+            if(typeof(action.payload.response)!=='undefined'){
+                return {
+                    ...state,
+                    statusCode:action.payload.response.status,
+                    errorMessage:action.payload.response.data.message,
+                    isError: true,
+                    isLoading: false
+                }
+            }
+            return {
+                ...state, isError: true, isLoading: false, isNetworkError: true,
+            }
+
+        // case get detail
+        case "GET_DETAIL_PENDING":
+            return {
+                ...state, isLoading: true
+            }
+
+        case "GET_DETAIL_FULFILLED":
+            return {
+                ...state,
+                isLoading: false,
+                isFinish: true,
+                regionDetail: action.payload.data
+            }
+
+        case "GET_DETAIL_REJECTED":
+            if(typeof(action.payload.response)!=='undefined'){
+                return {
+                    ...state,
+                    statusCode:action.payload.response.status,
+                    errorMessage:action.payload.response.data.message,
+                    isError: true,
+                    isLoading: false
+                }
+            }
+            return {
+                ...state, isError: true, isLoading: false, isNetworkError: true,
             }
 
         default:
