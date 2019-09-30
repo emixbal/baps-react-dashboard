53acc6a77c760ab5f79c5ba8821e03855ce77e73
iqbal <emixbal@gmail.com>
2019-09-30 10:10:35 +0700

    fix it ---> remove home

--- src/App.js
+++ src/App.js
@@ -7,20 +7,11 @@ import { store } from './app/redux/store'
 import Admin from "./app/layouts/Admin"
 import Auth from "./app/layouts/Auth"
 
-function Home(){
-    return(
-      <div>
-        Home
-      </div>
-    )
-}
-
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
           <Route path="/admin" render={props => <Admin {...props}/>} /> 
           <Route path='/auth' render={props => <Auth {...props}/>} />
           <Redirect to="/admin/dashboard" />
