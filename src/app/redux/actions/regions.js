95c56d4c110aca1723e27e1be107c300a9447513
iqbal <emixbal@gmail.com>
2019-09-30 10:09:44 +0700

    init region reducer

--- src/app/redux/actions/regions.js
+++ src/app/redux/actions/regions.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const IP = "https://reqres.in/api/users?page=2";
+import { IP } from '../../../config'
 
 export const fetchAll = () => ({
     type : 'FETCH_ALL',
-    payload : axios.get(IP)
+    payload : axios.get(IP, {headers:
+        {
+            'Content-Type':'application/json',
+            'Authorization':localStorage.getItem('tokenType')+' '+localStorage.getItem('token')
+        }
+    })
 });
