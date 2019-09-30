8db8a8f015cddaa5ee01448e550004f4b3b78c83
iqbal <emixbal@gmail.com>
2019-09-30 17:01:19 +0700

    fix region card

--- src/app/screens/regions/RegionCard.jsx
+++ src/app/screens/regions/RegionCard.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class RegionCard extends Component {
     render(){
         return(
             <div className="col-sm-3 mb-sm-3">
-                <div className="card">
-                    <div className="card-body">
-                        {this.props.name}
+                <Link to={"/admin/regions/detail/"+this.props.id}>
+                    <div className="card text-center">
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><i className="fa fa-flag"></i></li>
+                            <li className="list-group-item">{this.props.name}</li>
+                        </ul>
                     </div>
-                </div>
+                </Link>
             </div>
         )
     }
