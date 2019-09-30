3ca50f2afe80fcb2f830b5d3f750769b5acce544
iqbal <emixbal@gmail.com>
2019-09-30 17:00:26 +0700

    check token

--- src/app/layouts/Auth.js
+++ src/app/layouts/Auth.js
@@ -3,6 +3,15 @@ import React from 'react';
 import LoginScreen from "../screens/login";
 
 class Auth extends React.Component {
+    state = {
+        isRedirect : false
+    }
+    componentDidMount(){
+      if(localStorage.getItem('token')!=null){
+        this.setState({isRedirect:true})
+      }
+    }
+
     render() {
         return (
             <LoginScreen />
