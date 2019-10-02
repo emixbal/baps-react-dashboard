566c5921ba89572bb9a4eac172acacfa116abf07
iqbal <emixbal@gmail.com>
2019-10-02 10:41:49 +0700

    fic region action

--- src/app/redux/actions/region.js
+++ src/app/redux/actions/region.js
@@ -1,33 +1,23 @@
 import axios from 'axios';
 
-import { IP } from '../../../config'
+import { IP, axiosHeader } from '../../../config'
 
 export const fetchAll = () => ({
     type : 'FETCH_ALL',
-    payload : axios.get(IP+'/regions', {headers:
-        {
-            'Content-Type':'application/json',
-            'Authorization':localStorage.getItem('tokenType')+' '+localStorage.getItem('token')
-        }
-    })
+    payload : axios.get(`${IP}/regions`, {headers:axiosHeader})
 });
 
 export const create = (regionData) => ({
     type : 'CREATE',
-    payload : axios.post(IP+'/regions', regionData, {headers:
-        {
-            'Content-Type':'application/json',
-            'Authorization':localStorage.getItem('tokenType')+' '+localStorage.getItem('token')
-        }
-    })
+    payload : axios.post(`${IP}/regions`, regionData, {headers:axiosHeader})
 });
 
 export const getDetail = (id) => ({
     type : 'GET_DETAIL',
-    payload : axios.get(IP+'/regions/'+id, {headers:
-        {
-            'Content-Type':'application/json',
-            'Authorization':localStorage.getItem('tokenType')+' '+localStorage.getItem('token')
-        }
-    })
+    payload : axios.get(`${IP}/regions/${id}`, {headers:axiosHeader})
+});
+
+export const update = (id, regionData) => ({
+    type : 'UPDATE',
+    payload : axios.patch(`${IP}/regions/${id}`, regionData, {headers:axiosHeader})
 });
