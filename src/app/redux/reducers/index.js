0996f7123db4e068e2c942e310e093af43d12531
iqbal <emixbal@gmail.com>
2019-10-02 10:40:49 +0700

    fix region reducers

--- src/app/redux/reducers/index.js
+++ src/app/redux/reducers/index.js
@@ -3,13 +3,14 @@ import { combineReducers } from 'redux';
 // import all reducer
 import regionReducer from "./region";
 import authReducer from "./auth";
+import clusterReducer from "./cluster"
 
 
 const reducers = combineReducers({
     // router: reducerRouter,
     auth: authReducer,
-    region: regionReducer
-    
+    region: regionReducer,
+    cluster: clusterReducer,
 });
 
 export default reducers;
