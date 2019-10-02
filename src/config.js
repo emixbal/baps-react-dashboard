0daa6df166c716e9ff6a307dbfe283c9ea6ee29d
iqbal <emixbal@gmail.com>
2019-10-02 10:42:47 +0700

    add header http into config file

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
 // export const IP = "https://baps-adonis-server.herokuapp.com";
 export const IP = "http://127.0.0.1:3333";
+export const axiosHeader = {
+    'Content-Type':'application/json',
+    'Authorization':localStorage.getItem('tokenType')+' '+localStorage.getItem('token')
+}
