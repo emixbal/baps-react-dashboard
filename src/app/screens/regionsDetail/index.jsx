a58b4d34e6e021f130fb901969517db6990da562
iqbal <emixbal@gmail.com>
2019-10-03 15:41:06 +0700

    attach redux

--- src/app/screens/regionsDetail/index.jsx
+++ src/app/screens/regionsDetail/index.jsx
@@ -1,52 +1,121 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 // import components
-import RegionModalUpdate from './RegionModalUpdate'
+import RegionModalUpdate from './RegionModalUpdate';
+import AlertError from '../../components/AlertError';
 
 //import redux action
 import { getDetail } from '../../redux/actions/region';
+import { getClusterByRegionId } from '../../redux/actions/cluster';
 
 class RegionsDetailScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isActiveSwitch : false,
+        }
+    }
     componentDidMount() {
         this.props.dispatch(getDetail(this.props.match.params.id));
+        this.props.dispatch(getClusterByRegionId(this.props.match.params.id));
+    }
+
+    handleActiveSwitch(){
+        alert("sss")
+        // const region = {
+        //     "is_active":!this.props.region.regionDetail.is_active
+        // }
+        // await this.props.dispatch(update(this.props.match.params.id, region));
+        // await this.props.dispatch(getDetail(this.props.match.params.id));
     }
     
     render(){
         if(this.props.region.isError){
-            alert(this.props.region.errorMessage)
+            return(
+                <AlertError
+                    message={this.props.region.errorMessage}
+                    statusCode={this.props.region.statusCode}
+                />
+            )
         }
-        // segera di ganti dengan toast
-        if(this.props.region.isNetworkError){
-            alert("tidak dapat terhubung ke server")
-        }
-
-        const { regionDetail } = this.props.region
-
         if(this.props.region.isLoading){
             return(
-                <>
-                    <div>Loading...</div>
-                </>
+                <div>Loading...</div>
             );
         }
+
+        //destruct var
+        const { regionDetail } = this.props.region;
         
         return(
             <div>
                 <div className="row">
-                    <div className="offset-sm-10"><RegionModalUpdate buttonLabel="Update data" /></div>
+                    <div className="col-sm-10">
+                        <input className="btn" onChange={()=>alert("hallo")} />
+                        <div className="float-right">
+                            <div className="custom-control custom-switch">
+                                <input
+                                    type="checkbox"
+                                    className="custom-control-input"
+                                    id="customSwitches"
+                                    // checked={this.props.region.regionDetail.is_active}
+                                    // checked={this.state.isActiveSwitch}
+                                    checked={true}
+                                    onChange={()=>alert("hallo")}
+                                />
+                                <label className="custom-control-label">isActive?</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-2 float-right"><RegionModalUpdate buttonLabel="Update data" id={regionDetail.id} /></div>
                 </div>
                 <hr />
-                <div className="card">
+                <div className="card text-light bg-primary mb-sm-3">
                     <div className="card-body">
                         <div className="row">
                             <div className="col-sm-6">
-                                <p>Region Name: {regionDetail.name}</p>
-                                <p>Region: {regionDetail.id}</p>
+                                <p>Region Name: <strong>{regionDetail.name}</strong></p>
+                                <p>Region ID: <strong>{regionDetail.id}</strong></p>
                             </div>
                             <div className="col-sm-6">
-                                Description: {regionDetail.description}
+                                <p>Description: <strong>{regionDetail.description}</strong></p>
+                                <p>Towers Amount: 89</p>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10">
+                    <div className="card">
+                        <div className="card-header">
+                            Clusters:
+                        </div>
+                        <div className="card-body">
+                            <div className="row">
+                                {this.props.cluster.isLoading === true ? <div className="text-center">Loading...</div> :
+                                    <>
+                                    {this.props.cluster.data.length===0?<div className="col-sm-12">Belum Ada Cluster</div>:
+                                        <>
+                                        {
+                                            this.props.cluster.data.map(data=>
+                                                <div key={data.id} className="col-sm-3 mb-sm-1">
+                                                    <Link to="">
+                                                        <div className="card text-center">
+                                                            <ul className="list-group list-group-flush">
+                                                                <li className="list-group-item"><i className="fa fa-flag"></i></li>
+                                                                <li className="list-group-item">{data.name}</li>
+                                                            </ul>
+                                                        </div>
+                                                    </Link>
+                                                </div>
+                                            )
+                                        }
+                                        </>
+                                    }
+                                    </>                     
+                                }
+                            </div>    
                         </div>
                     </div>
                 </div>
@@ -56,7 +125,8 @@ class RegionsDetailScreen extends Component {
 }
 
 const mapStateToPros = state => ({
-    region: state.region
+    region: state.region,
+    cluster: state.cluster,
 })
 
 export default connect(mapStateToPros)(RegionsDetailScreen);
